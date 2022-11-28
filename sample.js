35b295df0e945f35ee21ae0527319cf304e13351
Mukta uday Awate <1000061974@hexaware.com>
2022-11-28 11:57:15 +0530

    code added

--- sample.js
+++ sample.js
@@ -1,1 +1,11 @@
-<h4>This is sample.js</h4>
+import React, { memo, useEffect, useState } from 'react';
+function sample() {
+    return (
+        <>
+        <div >
+           <h4>This is sample.js</h4>
+        </div>
+        </>
+    );
+  }
+  export default sample;
